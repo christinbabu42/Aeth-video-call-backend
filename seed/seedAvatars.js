4cd36afca249c55c1785dc8b8e2d952df3d77c8e
Christin Babu <christinbabu42@gmail.com>
2026-03-30 15:59:32 +0530

    add

--- seed/seedAvatars.js
+++ seed/seedAvatars.js
@@ -4,12 +4,12 @@ const Avatar = require("../models/Avatar");
 mongoose.connect("mongodb+srv://christinbabu42_db_user:JNtGNNu2QagQ3uZP@cluster0.p1foncl.mongodb.net/aeth_app?appName=Cluster0");
 
 const boys = Array.from({ length: 10 }).map((_, i) => ({
-  url: `/public/avathar-boy/${i + 1}.png`,
+  url: `/public/avathar-boy/${i + 1}.jpeg`, // ✅ FIXED
   gender: "boy",
 }));
 
 const girls = Array.from({ length: 10 }).map((_, i) => ({
-  url: `/public/avathar-girl/${i + 1}.png`,
+  url: `/public/avathar-girl/${i + 1}.jpeg`, // ✅ FIXED
   gender: "girl",
 }));
 
